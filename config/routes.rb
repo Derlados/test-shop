eecd97d89d11f39dd86ffd94363cc19016271f3f
Illia <derladoshome@gmail.com>
2023-02-26 17:40:39 +0200

    added devise module and sign-in/sign-up functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+    devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_up: 'reg', sign_out: 'logout' }
+    devise_scope :user do
+      get 'personal', to: 'devise/registrations#edit'
+    end
+
     root :to =>  redirect('/items')
 
     get '/items/storage', to: 'items#storage'
